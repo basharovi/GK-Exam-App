55e7ee9c515a026937dad89ea9a5923465d1fdb4
BasharOvi <bashar.ovi@outlook.com>
2020-12-29 21:27:56 +0600

    Merit List works fine

--- GKExamApp/Models/MeritViewModel.cs
+++ GKExamApp/Models/MeritViewModel.cs
@@ -2,9 +2,10 @@
 {
     public class MeritViewModel
     {
-        public int Positon { get; set; }
+        public int Position { get; set; }
         public string Name { get; set; }
         public string Username { get; set; }
+        public int TotalExam { get; set; }
         public decimal TotalScore { get; set; }
     }
 }
--- GKExamApp/UI/MeritList.xaml.cs
+++ GKExamApp/UI/MeritList.xaml.cs
@@ -2,10 +2,8 @@
 using GKExamApp.Models;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data.Entity;
 using System.Linq;
 using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Input;
 
 namespace GKExamApp.UI
@@ -28,7 +26,7 @@ namespace GKExamApp.UI
 
         private void BackButton_Click(object sender, RoutedEventArgs e)
         {
-            var dashboard = new AdminDashboard();
+            var dashboard = new UserDashboard();
             dashboard.Show();
             Hide();
         }
@@ -40,28 +38,43 @@ namespace GKExamApp.UI
 
         private void SearchByName_KeyUp(object sender, KeyEventArgs e)
         {
-            var name = SearchByNameTextBox.Text;
-            if (string.IsNullOrWhiteSpace(name))
-            {
-                ShowGrid.ItemsSource = _users;
-                return;
-            }
+            //var name = SearchByNameTextBox.Text;
+            //if (string.IsNullOrWhiteSpace(name))
+            //{
+            //    ShowGrid.ItemsSource = _users;
+            //    return;
+            //}
 
-            ShowGrid.ItemsSource = null;
-            ShowGrid.ItemsSource = _users.Where(x => x.Name.ToLower()
-            .Contains(name.ToLower()));
+            //ShowGrid.ItemsSource = null;
+            //ShowGrid.ItemsSource = _users.Where(x => x.Name.ToLower()
+            //.Contains(name.ToLower()));
         }
 
         private void BindUserList()
         {
+            var meritList = new List<MeritViewModel>();
+
             _users = _db.Users.Include("Exams").ToList();
+
+            foreach (var item in _users)
+            {
+                meritList.Add(new MeritViewModel
+                {
+                    Name = item.Name,
+                    Username = item.Username,
+                    TotalScore = item.Exams.Sum(x => x.Mark),
+                    TotalExam = item.Exams.Count
+                });
+            }
+
+            meritList = meritList.OrderByDescending(x => x.TotalScore).ToList();
 
             for (var i = 0; i < _users.Count; i++)
             {
-                _users[i].Index = i + 1;
+                meritList[i].Position = i + 1;
             }
 
-            ShowGrid.ItemsSource = _users;
+            ShowGrid.ItemsSource = meritList;
         }
     }
 }
