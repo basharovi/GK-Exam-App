0af53fb7493f3ddedc6ba02ef7f9c7ab03187522
BasharOvi <bashar.ovi@outlook.com>
2020-12-31 20:34:51 +0600

    Added Migration

---
+++ GKExamApp/Migrations/202012311433180_Updated Question Table.cs
@@ -0,0 +1,20 @@
+﻿namespace GKExamApp.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class UpdatedQuestionTable : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Questions", "Point", c => c.Decimal(nullable: false, precision: 18, scale: 2));
+            AddColumn("dbo.Questions", "TimeDuration", c => c.Int(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Questions", "TimeDuration");
+            DropColumn("dbo.Questions", "Point");
+        }
+    }
+}
